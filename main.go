35fe44cb1de08b4621780efb0625c98cf01b8ef8
Brock Tillotson <brocktillotson@6degreeshealth.com>
2019-04-29 08:45:12 -0700

    added a package

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/sajari/fuzzy"
 )
 
 func goodFunc() string {
@@ -15,4 +17,17 @@ func badFunc() string {
 func main() {
 	fmt.Printf("%+v\n", goodFunc())
 	fmt.Printf("%+v\n", badFunc())
+
+	model := fuzzy.NewModel()
+	model.SetThreshold(1)
+	model.SetDepth(5)
+	words := []string{"bob", "your", "uncle", "dynamite", "delicate", "biggest", "big", "bigger", "aunty", "you're"}
+	model.Train(words)
+
+	fmt.Println("\nSPELL CHECKS")
+	fmt.Println("	Deletion test (yor) : ", model.SpellCheck("yor"))
+	fmt.Println("	Swap test (uncel) : ", model.SpellCheck("uncel"))
+	fmt.Println("	Replace test (dynemite) : ", model.SpellCheck("dynemite"))
+	fmt.Println("	Insert test (dellicate) : ", model.SpellCheck("dellicate"))
+	fmt.Println("	Two char test (dellicade) : ", model.SpellCheck("dellicade"))
 }
