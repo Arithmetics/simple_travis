9325dd37d312e77720bb1aaf1e343a4104fdd14c
Brock Tillotson <brocktillotson@6degreeshealth.com>
2019-04-29 08:49:01 -0700

    test with package

--- main_test.go
+++ main_test.go
@@ -1,11 +1,27 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/sajari/fuzzy"
+)
 
 func TestGoodFunc(t *testing.T) {
 
 	if goodFunc() != "looks good" {
 		t.Error("ERROR")
+	}
+}
+
+func TestWords(t *testing.T) {
+	model := fuzzy.NewModel()
+	model.SetThreshold(1)
+	model.SetDepth(5)
+	words := []string{"bob", "your", "uncle", "dynamite", "delicate", "biggest", "big", "bigger", "aunty", "you're"}
+	model.Train(words)
+
+	if model.SpellCheck("yor") != "your" {
+		t.Error("yoooo bad")
 	}
 }
 
